a682e7bc89f716da211f936e9bba816559aa9e16
manifest <doslovno@gmail.com>
2017-11-08 01:36:00 +0300

    Add FlickrFetchr class

---
+++ app/src/main/java/com/example/manifest/photogallery/FlickrFetchr.java
@@ -0,0 +1,42 @@
+package com.example.manifest.photogallery;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FlickrFetchr {
+
+    /**
+     * получает низкоуровневые данные по URL и возвращает их в виде массива байтов.
+     */
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() +
+                        ": with " + urlSpec);
+            }
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    /**
+     * преобразует результат из getUrlBytes(String) в String .
+     */
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+}
