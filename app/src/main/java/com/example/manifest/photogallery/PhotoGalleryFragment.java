b97216ad25d3dee6185cf18c22dcb3b6323eb6b5
manifest <doslovno@gmail.com>
2017-11-08 19:59:03 +0300

    Add AsyncTask class

--- app/src/main/java/com/example/manifest/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/manifest/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,22 @@
 package com.example.manifest.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
 
     private RecyclerView mPhotoRecyclerView;
+
+    private static final String TAG = "PhotoGalleryFragment";
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -21,6 +27,7 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
         // включить удержание фрагмента(не будет уничтожаться при поворотах)
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -31,5 +38,19 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView = v.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr()
+                        .getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
